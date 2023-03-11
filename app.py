f2ebaa15bdbde2ee370a409982eb7fb33b77f314
Fujairah Research Center <108390870+FujairahResearchCenter@users.noreply.github.com>
2023-03-11 14:26:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ def hello():
     language = request.args.get('filter')
     response = {"data":[{"AppointmentId":3,"Text":"Isssnstall New Router in Dev Room","Description":None,"StartDate":"2021-04-26T21:30:00.000Z","EndDate":"2021-04-26T22:30:00.000Z","AllDay":False,"RecurrenceRule":None,"RecurrenceException":None}]}
     response = jsonify(response)
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', 'http://86.97.193.127:3000')
     return response
