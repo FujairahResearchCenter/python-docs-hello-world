bca8979c5125499ab5968ec39664868310c7b3d5
Fujairah Research Center <108390870+FujairahResearchCenter@users.noreply.github.com>
2023-03-11 11:52:45 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def hello():
     language = request.args.get('filter')
     response = {"data":[{"AppointmentId":3,"Text":"Isssnstall New Router in Dev Room","Description":None,"StartDate":"2021-04-26T21:30:00.000Z","EndDate":"2021-04-26T22:30:00.000Z","AllDay":False,"RecurrenceRule":None,"RecurrenceException":None}]}
-    response = flask.jsonify(response)
+    response = jsonify(response)
     response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
     return response
