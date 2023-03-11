50d8c51cfd7c599b7e5ee05cf50e1107b575ffbf
Fujairah Research Center <108390870+FujairahResearchCenter@users.noreply.github.com>
2023-03-11 11:30:06 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,jsonify
 app = Flask(__name__)
 
-@app.route('/Get',methods=['GET']")
+@app.route('/Get',methods=['GET'])
 def hello():
     language = request.args.get('filter')
     t = {"data":[{"AppointmentId":3,"Text":"Isssnstall New Router in Dev Room","Description":None,"StartDate":"2021-04-26T21:30:00.000Z","EndDate":"2021-04-26T22:30:00.000Z","AllDay":False,"RecurrenceRule":None,"RecurrenceException":None}]}
